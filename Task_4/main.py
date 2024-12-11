a088ac3ee125f060c358585138a4f9786428d4f8
Ballist166 <40292866+Ballist166@users.noreply.github.com>
2024-12-11 19:18:54 +0500

    init

--- Task_4/main.py
+++ Task_4/main.py
@@ -80,57 +80,46 @@ class Cart:
             print(f"{product.name} {product.price} {quantity} {total_price}")
         print(f"---Total: {self.total}---")
 
-
-# Пример использования
 billy = User('billy@rambler.ru')
 
 lemon = Product('lemon', 20)
 carrot = Product('carrot', 30)
 
 cart_billy = Cart(billy)
-print(cart_billy.user)  # Пользователь billy@rambler.ru, баланс - 0
-
-# Добавляем товары в корзину
+print(cart_billy.user) # Пользователь billy@rambler.ru, баланс - 0
 cart_billy.add(lemon, 2)
-cart_billy.add(carrot, 1)
+cart_billy.add(carrot)
 cart_billy.print_check()
-# Печатает текст ниже
-# ---Your check---
-# carrot 30 1 30
-# lemon 20 2 40
-# ---Total: 70---
-
+''' Печатает текст ниже
+---Your check---
+carrot 30 1 30
+lemon 20 2 40
+---Total: 70---'''
 cart_billy.add(lemon, 3)
 cart_billy.print_check()
-# Печатает текст ниже
-# ---Your check---
-# carrot 30 1 30
-# lemon 20 5 100
-# ---Total: 130---
-
+''' Печатает текст ниже
+---Your check---
+carrot 30 1 30
+lemon 20 5 100
+---Total: 130---'''
 cart_billy.remove(lemon, 6)
 cart_billy.print_check()
-# Печатает текст ниже
-# ---Your check---
-# carrot 30 1 30
-# ---Total: 30---
-
-print(cart_billy.total)  # 30
-
+''' Печатает текст ниже
+---Your check---
+carrot 30 1 30
+---Total: 30---'''
+print(cart_billy.total) # 30
 cart_billy.add(lemon, 5)
 cart_billy.print_check()
-# Печатает текст ниже
-# ---Your check---
-# carrot 30 1 30
-# lemon 20 5 100
-# ---Total: 130---
-
+''' Печатает текст ниже
+---Your check---
+carrot 30 1 30
+lemon 20 5 100
+---Total: 130---'''
 cart_billy.order()
-# Печатает текст ниже
-# Не хватает средств на балансе. Пополните счет
-# Проблема с оплатой
-
-cart_billy.user.deposit(150)  # Пополнение баланса
-cart_billy.order()  # Заказ оплачен
-print(cart_billy.user.balance)  # 20
-
+''' Печатает текст ниже
+Не хватает средств на балансе. Пополните счет
+Проблема с оплатой'''
+cart_billy.user.deposit(150)
+cart_billy.order() # Заказ оплачен
+print(cart_billy.user.balance) # 20
